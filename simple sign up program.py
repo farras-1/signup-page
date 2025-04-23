382c6621ce44ea52f22ed8d1cd5c62daea224cde
farras-1 <farrasrazan05@gmail.com>
2025-04-23 21:48:06 +0700

    Update simple sign up program.py
    menambahkan opsi menu seperti sign up dan login

--- simple sign up program.py
+++ simple sign up program.py
@@ -1,33 +1,51 @@
 accounts = {}
-
-print("========please signup your account========")
-try:
-    user = input("please input your username :")
-    while user == "":
-        print("please fill the username")
+def signup():
+    try:
         user = input("please input your username :")
+        while user == "":
+            print("please fill the username")
+            user = input("please input your username :")
 
-    userpass = input("please input your password :")
-    while userpass == "":
-        print("please fill the password")
         userpass = input("please input your password :")
+        while userpass == "":
+            print("please fill the password")
+            userpass = input("please input your password :")
 
-    accounts[user] = userpass
-    print("account successfully created")
-except:
-    print("account failed to create, please try again")
-print("========please verify your account========")
-userverif = input("please input your username :")
-while userverif == "":
-    print("please fill your username")
+        accounts[user] = userpass
+        print("account successfully created")
+    except:
+        print("account failed to create, please try again")
+
+def login():
+    
     userverif = input("please input your username :")
+    while userverif == "":
+        print("please fill your username")
+        userverif = input("please input your username :")
 
-passverif = input("please input your password :")
-while passverif == "":
-    print("please fill your password")
     passverif = input("please input your password :")
+    while passverif == "":
+        print("please fill your password")
+        passverif = input("please input your password :")
 
-if userverif in accounts and accounts[userverif] == passverif:
-    print("account verification succeed")
-else:
-    print("password or username incorrect")
+    if userverif in accounts and accounts[userverif] == passverif:
+        print("account verification succeed")
+    else:
+        print("password or username incorrect")
+        
+while True:
+    print("opsi :")
+    print("sign up")
+    print("login")
+    print("keluar")
+
+    opsi = input("masukkan opsi : ")
+
+    if opsi == "sign up":
+        signup()
+    elif opsi == "login":
+        login()
+    elif opsi == "keluar":
+        break
+    else :
+        print("opsi tidak valid")
